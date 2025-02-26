88a4a3cc9f707395d804d37adeafdeb69611e87a
vincilisi <lisitano95@gmail.com>
2025-02-26 22:19:56 +0100

    inizio navabar

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,16 +3,42 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PokeDex</title>
+    <title>HomePage</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body data-logout-url="{{ route('logout') }}">
+    @include('components.nav')
     <h1>Benvenuto alla Homepage!</h1>
-    <form method="POST" action="{{ route('logout') }}">
+
+
+
+
+
+
+
+
+
+    <!-- <form method="POST" action="{{ route('logout') }}">
         @csrf
         <button type="submit">Logout</button>
-    </form>
-    <div id="countdown">Logout tra 5 secondi</div>
+    </form> -->
+
+    <!-- <div id="countdown">Logout tra 5 secondi</div> -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <script src="{{ asset('js/tempo.js') }}"></script>
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
 </html>
